b980b3583be4c2cf603d3df22370e68795216d20
janka000 <14cernikova@gmail.com>
2019-07-28 16:07:35 +0200

    merged projects

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+})
+function click(value) {
+    alert('clicked on ' + value);
+}
+function click10() {
+    alert('clicked on 10 (I will add funtionality later in back-end) ');
+}
+function click20() {
+    alert('clicked on 20 (I will add funtionality later in back-end)');
+}
+function click50() {
+    alert('clicked on 50 (I will add funtionality later in back-end)');
+}
