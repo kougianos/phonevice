a39d00459e4ccc05a9f8911d662f1b8c634515e7
Nikos Kougianos <kougi_jr@hotmail.com>
2019-07-30 14:34:57 +0300

    Fix console error

--- data/translate.sh
+++ data/translate.sh
@@ -3,6 +3,6 @@
 php -f ./tsmarty2c.php ../templates/*.html > ./smarty.c
 
 
-# en_GB
-xgettext --no-location --no-wrap --join-existing -L C --force-po --foreign-user --default-domain=PhoneVice --copyright-holder="PhoneVice" --package-name="PhoneVice" --from-code=UTF-8 --directory=. --output-dir=../locale/en_GB/LC_MESSAGES/ ./smarty.c
-msgfmt --output-file=../locale/en_GB/LC_MESSAGES/phonevice.mo ../locale/en_GB/LC_MESSAGES/phonevice.po
+# el_GR
+xgettext --no-location --no-wrap --join-existing -L C --force-po --foreign-user --default-domain=PhoneVice --copyright-holder="PhoneVice" --package-name="PhoneVice" --from-code=UTF-8 --directory=. --output-dir=../locale/el_GR/LC_MESSAGES/ ./smarty.c
+msgfmt --output-file=../locale/el_GR/LC_MESSAGES/phonevice.mo ../locale/el_GR/LC_MESSAGES/phonevice.po
--- js/custom.js
+++ js/custom.js
@@ -1,59 +1,66 @@
+$(document).ready(function () {
 
-document.getElementById("+/-$10").onclick = function () {
-	TenRange()
-};
+	if (document.getElementById('budget_page')) {
 
-function TenRange() {
-	document.getElementById("range").value = '10';
-	document.getElementById("+/-$10").innerHTML = '+/-$10 <img src="./img/tick.png" style="height: 10px; display: inline;">';
-	document.getElementById("+/-$10").classList.add('blacktext');
-	document.getElementById("+/-$10").classList.remove('graytext');
-	document.getElementById("+/-$20").innerHTML = '+/-$20 <img src="./img/tick.png" style="height: 10px; display: none;">';
-	document.getElementById("+/-$20").classList.add('graytext');
-	document.getElementById("+/-$20").classList.remove('blacktext');
-	document.getElementById("+/-$50").innerHTML = '+/-$50 <img src="./img/tick.png" style="height: 10px; display: none;">';
-	document.getElementById("+/-$50").classList.add('graytext');
-	document.getElementById("+/-$50").classList.remove('blacktext');
-}
+		document.getElementById("+/-$10").onclick = function () {
+			TenRange()
+		};
 
-document.getElementById("+/-$20").onclick = function () {
-	TwentyRange()
-};
+		function TenRange() {
+			document.getElementById("range").value = '10';
+			document.getElementById("+/-$10").innerHTML = '+/-$10 <img src="./img/tick.png" style="height: 10px; display: inline;">';
+			document.getElementById("+/-$10").classList.add('blacktext');
+			document.getElementById("+/-$10").classList.remove('graytext');
+			document.getElementById("+/-$20").innerHTML = '+/-$20 <img src="./img/tick.png" style="height: 10px; display: none;">';
+			document.getElementById("+/-$20").classList.add('graytext');
+			document.getElementById("+/-$20").classList.remove('blacktext');
+			document.getElementById("+/-$50").innerHTML = '+/-$50 <img src="./img/tick.png" style="height: 10px; display: none;">';
+			document.getElementById("+/-$50").classList.add('graytext');
+			document.getElementById("+/-$50").classList.remove('blacktext');
+		}
 
-function TwentyRange() {
-	document.getElementById("range").value = '20';
-	document.getElementById("+/-$20").innerHTML = '+/-$20 <img src="./img/tick.png" style="height: 10px; display: inline;">';
-	document.getElementById("+/-$20").classList.add('blacktext');
-	document.getElementById("+/-$20").classList.remove('graytext');
-	document.getElementById("+/-$10").innerHTML = '+/-$10 <img src="./img/tick.png" style="height: 10px; display: none;">';
-	document.getElementById("+/-$10").classList.add('graytext');
-	document.getElementById("+/-$10").classList.remove('blacktext');
-	document.getElementById("+/-$50").innerHTML = '+/-$50 <img src="./img/tick.png" style="height: 10px; display: none;">';
-	document.getElementById("+/-$50").classList.add('graytext');
-	document.getElementById("+/-$50").classList.remove('blacktext');
-}
+		document.getElementById("+/-$20").onclick = function () {
+			TwentyRange()
+		};
 
-document.getElementById("+/-$50").onclick = function () {
-	FiftyRange()
-};
+		function TwentyRange() {
+			document.getElementById("range").value = '20';
+			document.getElementById("+/-$20").innerHTML = '+/-$20 <img src="./img/tick.png" style="height: 10px; display: inline;">';
+			document.getElementById("+/-$20").classList.add('blacktext');
+			document.getElementById("+/-$20").classList.remove('graytext');
+			document.getElementById("+/-$10").innerHTML = '+/-$10 <img src="./img/tick.png" style="height: 10px; display: none;">';
+			document.getElementById("+/-$10").classList.add('graytext');
+			document.getElementById("+/-$10").classList.remove('blacktext');
+			document.getElementById("+/-$50").innerHTML = '+/-$50 <img src="./img/tick.png" style="height: 10px; display: none;">';
+			document.getElementById("+/-$50").classList.add('graytext');
+			document.getElementById("+/-$50").classList.remove('blacktext');
+		}
 
-function FiftyRange() {
-	document.getElementById("range").value = '50';
-	document.getElementById("+/-$50").innerHTML = '+/-$50 <img src="./img/tick.png" style="height: 10px; display: inline;">';
-	document.getElementById("+/-$50").classList.add('blacktext');
-	document.getElementById("+/-$50").classList.remove('graytext');
-	document.getElementById("+/-$10").innerHTML = '+/-$10 <img src="./img/tick.png" style="height: 10px; display: none;">';
-	document.getElementById("+/-$10").classList.add('graytext');
-	document.getElementById("+/-$10").classList.remove('blacktext');
-	document.getElementById("+/-$20").innerHTML = '+/-$20 <img src="./img/tick.png" style="height: 10px; display: none;">';
-	document.getElementById("+/-$20").classList.add('graytext');
-	document.getElementById("+/-$20").classList.remove('blacktext');
-}
+		document.getElementById("+/-$50").onclick = function () {
+			FiftyRange()
+		};
 
-document.getElementById("submit").onclick = function () {
-	SubmitForm();
-};
+		function FiftyRange() {
+			document.getElementById("range").value = '50';
+			document.getElementById("+/-$50").innerHTML = '+/-$50 <img src="./img/tick.png" style="height: 10px; display: inline;">';
+			document.getElementById("+/-$50").classList.add('blacktext');
+			document.getElementById("+/-$50").classList.remove('graytext');
+			document.getElementById("+/-$10").innerHTML = '+/-$10 <img src="./img/tick.png" style="height: 10px; display: none;">';
+			document.getElementById("+/-$10").classList.add('graytext');
+			document.getElementById("+/-$10").classList.remove('blacktext');
+			document.getElementById("+/-$20").innerHTML = '+/-$20 <img src="./img/tick.png" style="height: 10px; display: none;">';
+			document.getElementById("+/-$20").classList.add('graytext');
+			document.getElementById("+/-$20").classList.remove('blacktext');
+		}
 
-function SubmitForm() {
-	document.getElementById("form").submit();
-}
+		document.getElementById("submit").onclick = function () {
+			SubmitForm();
+		};
+
+		function SubmitForm() {
+			document.getElementById("form").submit();
+		}
+
+	}
+	
+});
