7992fd4a11acc08d847aed95cee14276b22643a5
Nikos Kougianos <kougi_jr@hotmail.com>
2019-10-15 16:26:40 +0300

    Translation setup

--- data/translate.sh
+++ data/translate.sh
@@ -4,5 +4,5 @@ php -f ./tsmarty2c.php ../templates/*.html > ./smarty.c
 
 
 # el_GR
-xgettext --no-location --no-wrap --join-existing -L C --force-po --foreign-user --default-domain=PhoneVice --copyright-holder="PhoneVice" --package-name="PhoneVice" --from-code=UTF-8 --directory=. --output-dir=../locale/el_GR/LC_MESSAGES/ ./smarty.c
+xgettext --no-location --no-wrap --join-existing -L C --force-po --foreign-user --default-domain=phonevice --copyright-holder="phonevice" --package-name="phonevice" --from-code=UTF-8 --directory=. --output-dir=../locale/el_GR/LC_MESSAGES/ ./smarty.c
 msgfmt --output-file=../locale/el_GR/LC_MESSAGES/phonevice.mo ../locale/el_GR/LC_MESSAGES/phonevice.po
